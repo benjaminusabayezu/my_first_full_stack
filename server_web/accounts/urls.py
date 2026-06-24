ad85df2913aa3ea87958ed697674b508fc7ae3b8
Benjamin <benjamin96usab@gmail.com>
2026-06-24 19:38:57 +0200

    fix temporaly admin creator

--- server_web/accounts/urls.py
+++ server_web/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView,LoginView
+from .views import RegisterView,LoginView, CreateAdminView
 from rest_framework_simplejwt.views import(
     
     TokenRefreshView
@@ -9,4 +9,5 @@ urlpatterns = [
      path('register/',RegisterView.as_view(), name='register'),
      path('login/',LoginView.as_view(), name='login'),
      path('refresh/',TokenRefreshView.as_view(), name='refresh'),
+     path("create_admin/",CreateAdminView.as_view()),
 ]
--- server_web/accounts/views.py
+++ server_web/accounts/views.py
@@ -1,28 +1,61 @@
-
 from rest_framework import generics
 from .models import User
 from .serializers import RegisterSerializer
 
-#for login 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .serializers import LoginSerializer
 
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
 
 class RegisterView(generics.CreateAPIView):
-    queryset =User.objects.all()
-    serializer_class =RegisterSerializer
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
 
 class LoginView(APIView):
-    def post(self ,request):
+    def post(self, request):
         serializer = LoginSerializer(
             data=request.data
         )
+
         serializer.is_valid(
             raise_exception=True
         )
+
         return Response(
             serializer.validated_data
         )
+
+
+# TEMPORARY: create admin on Render without Shell
+class CreateAdminView(APIView):
+
+    def post(self, request):
+
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+
+        if User.objects.filter(username=username).exists():
+            return Response({
+                "message": "User already exists"
+            })
+
+
+        user = User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+
+        return Response({
+            "message": "Admin created",
+            "username": user.username
+        })

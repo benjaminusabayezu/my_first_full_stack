2a42eb921a043d6c9c262317e0e8426f4aa45287
Benjamin <benjamin96usab@gmail.com>
2026-06-25 02:52:03 +0200

    removing admin creation  files

--- server_web/accounts/urls.py
+++ server_web/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.response import Response
-from .views import DBCheckView, RegisterView,LoginView, CreateAdminView,DebugLoginView , PasswordCheckView,CheckUserRoleView
+from .views import  RegisterView,LoginView
 from rest_framework_simplejwt.views import(
     
     TokenRefreshView
@@ -10,11 +10,11 @@ urlpatterns = [
      path('register/',RegisterView.as_view(), name='register'),
      path('login/',LoginView.as_view(), name='login'),
      path('refresh/',TokenRefreshView.as_view(), name='refresh'),
-     path("create-admin/",CreateAdminView.as_view()),
-     path("test/", lambda request: Response({"ok":"yes"})),
+    #  path("create-admin/",CreateAdminView.as_view()),
+    #  path("test/", lambda request: Response({"ok":"yes"})),
 
-     path('debug-login/',DebugLoginView.as_view() ),
-     path("password-check/", PasswordCheckView.as_view()),
-     path("check-role/", CheckUserRoleView.as_view()),
-     path("db-check/", DBCheckView.as_view()),
+    #  path('debug-login/',DebugLoginView.as_view() ),
+    #  path("password-check/", PasswordCheckView.as_view()),
+    #  path("check-role/", CheckUserRoleView.as_view()),
+    #  path("db-check/", DBCheckView.as_view()),
 ]
--- server_web/accounts/views.py
+++ server_web/accounts/views.py
@@ -36,92 +36,92 @@ class LoginView(APIView):
 
 
 # TEMPORARY: create admin on Render without Shell
-class CreateAdminView(APIView):
+# class CreateAdminView(APIView):
 
-    def post(self, request):
+#     def post(self, request):
 
-        try:
-            username = request.data.get("username")
-            email = request.data.get("email")
-            password = request.data.get("password")
+#         try:
+#             username = request.data.get("username")
+#             email = request.data.get("email")
+#             password = request.data.get("password")
 
-            user, created = User.objects.get_or_create(
-                username=username
-            )
+#             user, created = User.objects.get_or_create(
+#                 username=username
+#             )
 
-            user.email = email
-            user.role = "admin"
-            user.is_staff = True
-            user.is_superuser = True
-            user.is_active = True
+#             user.email = email
+#             user.role = "admin"
+#             user.is_staff = True
+#             user.is_superuser = True
+#             user.is_active = True
 
-            if password:
-                user.set_password(password)
+#             if password:
+#                 user.set_password(password)
 
-            user.save()
+#             user.save()
 
-            return Response({
-                "message": "Admin updated",
-                "username": user.username,
-                "role": user.role,
-                "created": created
-            })
+#             return Response({
+#                 "message": "Admin updated",
+#                 "username": user.username,
+#                 "role": user.role,
+#                 "created": created
+#             })
 
-        except Exception as e:
-            return Response({
-                "error": str(e)
-            }, status=500)
+#         except Exception as e:
+#             return Response({
+#                 "error": str(e)
+#             }, status=500)
     
-class DebugLoginView(APIView):
-    def post(self, request):
+# class DebugLoginView(APIView):
+#     def post(self, request):
 
-        username = request.data.get("username")
-        password = request.data.get("password")
+#         username = request.data.get("username")
+#         password = request.data.get("password")
 
-        user = authenticate(
-            username=username,
-            password=password
-        )
+#         user = authenticate(
+#             username=username,
+#             password=password
+#         )
 
-        return Response({
-            "authenticated": user is not None,
-            "username_received": username,
-            "user": str(user)
-        })
+#         return Response({
+#             "authenticated": user is not None,
+#             "username_received": username,
+#             "user": str(user)
+#         })
     
-class PasswordCheckView(APIView):
-           def post(self, request):
+# class PasswordCheckView(APIView):
+#            def post(self, request):
 
-              username = request.data.get("username")
-              password = request.data.get("password")
+#               username = request.data.get("username")
+#               password = request.data.get("password")
 
-              user = User.objects.get(username=username)
+#               user = User.objects.get(username=username)
 
-              return Response({
-               "username": user.username,
-               "stored_password": user.password[:20],
-               "check_password": user.check_password(password),
-               "is_active": user.is_active,
-        })
-class CheckUserRoleView(APIView):
-    def get(self, request):
+#               return Response({
+#                "username": user.username,
+#                "stored_password": user.password[:20],
+#                "check_password": user.check_password(password),
+#                "is_active": user.is_active,
+#         })
+# class CheckUserRoleView(APIView):
+#     def get(self, request):
 
-        users = User.objects.all().values(
-            "id",
-            "username",
-            "email",
-            "role",
-            "is_superuser"
-        )
+#         users = User.objects.all().values(
+#             "id",
+#             "username",
+#             "email",
+#             "role",
+#             "is_superuser"
+#         )
 
-        return Response(list(users))
+#         return Response(list(users))
 
-class DBCheckView(APIView):
-    def get(self, request):
+# class DBCheckView(APIView):
+#     def get(self, request):
 
-        return Response({
-            "NAME": connection.settings_dict["NAME"],
-            "HOST": connection.settings_dict["HOST"],
-            "USER": connection.settings_dict["USER"],
-            "PORT": connection.settings_dict["PORT"],
-        })
+#         return Response({
+#             "NAME": connection.settings_dict["NAME"],
+#             "HOST": connection.settings_dict["HOST"],
+#             "USER": connection.settings_dict["USER"],
+#             "PORT": connection.settings_dict["PORT"],
+#         })
